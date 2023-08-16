52215967b9f1d04172402c1251b5fb7f5239aa54
Xyvin Ashin <admin@xyvin.com>
2023-08-16 15:02:39 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './units/navbar/navbar.component';
+import { FooterComponent } from './units/footer/footer.component';
+import { ContactSectionComponent } from './units/contact-section/contact-section.component';
+import { TestimonialsComponent } from './units/testimonials/testimonials.component';
+import { PartnersComponent } from './units/partners/partners.component';
+import { ServicesComponent } from './units/services/services.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    ContactSectionComponent,
+    TestimonialsComponent,
+    PartnersComponent,
+    ServicesComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
