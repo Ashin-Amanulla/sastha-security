c58970f82f7847ef48d9b212b7a9779bccc039e8
Ashin Amanulla <141544499+XyvinTech@users.noreply.github.com>
2024-10-23 23:43:11 +0530

    new

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,24 @@
-import { Component, ElementRef, Renderer2 } from '@angular/core';
+import { Component,  } from '@angular/core';
+declare var bootstrap: any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+constructor() { }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.showModal();
+    }, 1500);
+  }
+
+  showModal(): void {
+    const modalElement = document.getElementById('imageModal');
+    const modal = new bootstrap.Modal(modalElement); // Initialize modal
+    modal.show(); // Show modal on page load
+  }
 
 
-  
 }
